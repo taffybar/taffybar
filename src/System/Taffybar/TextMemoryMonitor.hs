5185347eae16fa573a290aa82d36539ca4f63921
Tristan Ravitch <travitch@cs.wisc.edu>
2014-04-24 21:38:20 -0700

    Warning cleanup

--- src/System/Taffybar/TextMemoryMonitor.hs
+++ src/System/Taffybar/TextMemoryMonitor.hs
@@ -1,28 +1,29 @@
 module System.Taffybar.TextMemoryMonitor(textMemoryMonitorNew) where
 
-import Text.Printf
-import Text.StringTemplate
+import qualified Text.StringTemplate as ST
 import System.Information.Memory
-import System.Taffybar.Widgets.PollingLabel
-import Graphics.UI.Gtk
+import System.Taffybar.Widgets.PollingLabel ( pollingLabelNew )
+import qualified Graphics.UI.Gtk as Gtk
 
 -- | Creates a simple textual memory monitor. It updates once every polling
 -- period (in seconds).
 textMemoryMonitorNew :: String -- ^ Format. You can use variables: "used", "total", "free", "buffer", "cache", "rest", "used".
                      -> Double -- ^ Polling period in seconds.
-                     -> IO Widget
+                     -> IO Gtk.Widget
 textMemoryMonitorNew fmt period = do
     label <- pollingLabelNew fmt period callback
-    widgetShowAll label
+    Gtk.widgetShowAll label
     return label
     where
       callback = do
-        info <- parseMeminfo 
-        let template = newSTMP fmt
+        info <- parseMeminfo
+        let template = ST.newSTMP fmt
         let labels = ["used", "total", "free", "buffer", "cache", "rest", "used"]
         let actions = [memoryUsed, memoryTotal, memoryFree, memoryBuffer, memoryCache, memoryRest]
-        let actions' = map ((show.round).) actions
+            actions' = map ((show . intRound).) actions
         let stats = [f info | f <- actions']
-        let template' = setManyAttrib (zip labels stats) template
-        return $ render template'
+        let template' = ST.setManyAttrib (zip labels stats) template
+        return $ ST.render template'
 
+intRound :: Double -> Int
+intRound = round
