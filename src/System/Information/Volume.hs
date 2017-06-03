25619d4107e979d6d0b516c94d550842fb305830
Nick Hu <me@nickhu.co.uk>
2017-06-03 21:19:24 +0100

    Compatability with newer alsa-mixer library

--- src/System/Information/Volume.hs
+++ src/System/Information/Volume.hs
@@ -1,5 +1,4 @@
 module System.Information.Volume (
-  -- * Accessors
   getVolume,
   setVolume
 ) where
@@ -8,30 +7,31 @@ import Sound.ALSA.Mixer
 
 -- | Gets volume of a channel of a mixer
 getVolume :: String -> String -> IO Integer
-getVolume mixer channel = do
+getVolume mix channel = withMixer mix $ \mixer -> do
   Just control <- getControlByName mixer channel
   let Just playbackVolume = playback $ volume control
   (lo, hi) <- getRange playbackVolume
-  Just vol <- getChannel FrontLeft $ value $ playbackVolume
+  Just vol <- getChannel FrontLeft $ value playbackVolume
   return $ toPercent vol lo hi
 
 -- | Sets volume of a channel of a mixer
 setVolume :: String -> String -> Double -> IO ()
-setVolume mixer channel vol = do
-   Just control <- getControlByName mixer channel
-   let Just playbackVolume = playback $ volume control
-   (lo, hi) <- getRange playbackVolume
-   setChannel FrontLeft (value $ playbackVolume) $ fromPercent vol lo hi
-   setChannel FrontRight (value $ playbackVolume) $ fromPercent vol lo hi
-   setChannel RearLeft (value $ playbackVolume) $ fromPercent vol lo hi
-   setChannel RearRight (value $ playbackVolume) $ fromPercent vol lo hi
-   setChannel FrontCenter (value $ playbackVolume) $ fromPercent vol lo hi
+setVolume mix channel vol = withMixer mix $ \mixer -> do
+  Just control <- getControlByName mixer channel
+  let Just playbackVolume = playback $ volume control
+  (lo, hi) <- getRange playbackVolume
+  setChannel FrontLeft (value playbackVolume) $ fromPercent vol lo hi
+  setChannel FrontRight (value playbackVolume) $ fromPercent vol lo hi
+  setChannel RearLeft (value playbackVolume) $ fromPercent vol lo hi
+  setChannel RearRight (value playbackVolume) $ fromPercent vol lo hi
+  setChannel FrontCenter (value playbackVolume) $ fromPercent vol lo hi
 
 toPercent :: Integer -> Integer -> Integer -> Integer
 toPercent v lo hi = ceiling $ (v' - lo') / (hi' - lo') * 100
   where v' = fromIntegral v
         lo' = fromIntegral lo
         hi' = fromIntegral hi
+
 fromPercent :: Double -> Integer -> Integer -> Integer
 fromPercent v lo hi = ceiling $ lo' + (hi' - lo') * v / 100
   where lo' = fromIntegral lo
--- src/System/Taffybar/Volume.hs
+++ src/System/Taffybar/Volume.hs
@@ -7,22 +7,22 @@ import System.Information.Volume
 import System.Taffybar.Widgets.PollingLabel
 import Graphics.UI.Gtk
 
+-- | Creates a new text volume meter
 volumeTextNew :: String
                  -> String
                  -> Double
                  -> IO Widget
 volumeTextNew mixer control pollSeconds = do
-  l <- pollingLabelNew "" pollSeconds ((getVolume mixer control >>= return . show))
+  l <- pollingLabelNew "" pollSeconds . fmap show $ getVolume mixer control
   widgetShowAll l
   return l
 
+-- | Creates a new volume meter widget
 volumeControlNew :: String -> String -> IO Widget
 volumeControlNew mixer control = do
   b <- volumeButtonNew
-
-  _ <- on b scaleButtonValueChanged $ \v -> do
+  _ <- on b scaleButtonValueChanged $ \v ->
     setVolume mixer control (v * 100)
-
   let w = toWidget b
   widgetShowAll w
   return w
