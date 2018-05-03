fa16fdf1a27177e4ee39e5f53113be6b923642e4
Ivan Malison <IvanMalison@gmail.com>
2018-05-03 10:31:12 -0700

    Make haskell style in Information.Battery consistent

--- src/System/Taffybar/Information/Battery.hs
+++ src/System/Taffybar/Information/Battery.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
--- | This is a simple library to query the Linux UPower daemon (via
--- DBus) for battery information.  Currently, it only retrieves
--- information for the first battery it finds.
+-- | This is a simple library to query the Linux UPower daemon (via DBus) for
+-- battery information. Currently, it only retrieves information for the first
+-- battery it finds.
 module System.Taffybar.Information.Battery (
   -- * Types
   BatteryContext,
@@ -29,66 +29,71 @@ import Safe ( atMay )
 -- | An opaque wrapper around some internal library state
 data BatteryContext = BC Client ObjectPath
 
-data BatteryType = BatteryTypeUnknown
-                 | BatteryTypeLinePower
-                 | BatteryTypeBatteryType
-                 | BatteryTypeUps
-                 | BatteryTypeMonitor
-                 | BatteryTypeMouse
-                 | BatteryTypeKeyboard
-                 | BatteryTypePda
-                 | BatteryTypePhone
-                 deriving (Show, Ord, Eq, Enum)
+data BatteryType
+  = BatteryTypeUnknown
+  | BatteryTypeLinePower
+  | BatteryTypeBatteryType
+  | BatteryTypeUps
+  | BatteryTypeMonitor
+  | BatteryTypeMouse
+  | BatteryTypeKeyboard
+  | BatteryTypePda
+  | BatteryTypePhone
+  deriving (Show, Ord, Eq, Enum)
 
-data BatteryState = BatteryStateUnknown
-                  | BatteryStateCharging
-                  | BatteryStateDischarging
-                  | BatteryStateEmpty
-                  | BatteryStateFullyCharged
-                  | BatteryStatePendingCharge
-                  | BatteryStatePendingDischarge
-                  deriving (Show, Ord, Eq, Enum)
+data BatteryState
+  = BatteryStateUnknown
+  | BatteryStateCharging
+  | BatteryStateDischarging
+  | BatteryStateEmpty
+  | BatteryStateFullyCharged
+  | BatteryStatePendingCharge
+  | BatteryStatePendingDischarge
+  deriving (Show, Ord, Eq, Enum)
 
-data BatteryTechnology = BatteryTechnologyUnknown
-                       | BatteryTechnologyLithiumIon
-                       | BatteryTechnologyLithiumPolymer
-                       | BatteryTechnologyLithiumIronPhosphate
-                       | BatteryTechnologyLeadAcid
-                       | BatteryTechnologyNickelCadmium
-                       | BatteryTechnologyNickelMetalHydride
-                       deriving (Show, Ord, Eq, Enum)
+data BatteryTechnology
+  = BatteryTechnologyUnknown
+  | BatteryTechnologyLithiumIon
+  | BatteryTechnologyLithiumPolymer
+  | BatteryTechnologyLithiumIronPhosphate
+  | BatteryTechnologyLeadAcid
+  | BatteryTechnologyNickelCadmium
+  | BatteryTechnologyNickelMetalHydride
+  deriving (Show, Ord, Eq, Enum)
 
 -- | There are a few fields supported by UPower that aren't exposed
 -- here.. could be easily.
-data BatteryInfo = BatteryInfo { batteryNativePath :: Text
-                               , batteryVendor :: Text
-                               , batteryModel :: Text
-                               , batterySerial :: Text
-                               -- , batteryUpdateTime :: Time
-                               , batteryType :: BatteryType
-                               , batteryPowerSupply :: Bool
-                               , batteryHasHistory :: Bool
-                               , batteryHasStatistics :: Bool
-                               , batteryOnline :: Bool
-                               , batteryEnergy :: Double
-                               , batteryEnergyEmpty :: Double
-                               , batteryEnergyFull :: Double
-                               , batteryEnergyFullDesign :: Double
-                               , batteryEnergyRate :: Double
-                               , batteryVoltage :: Double
-                               , batteryTimeToEmpty :: Int64
-                               , batteryTimeToFull :: Int64
-                               , batteryPercentage :: Double
-                               , batteryIsPresent :: Bool
-                               , batteryState :: BatteryState
-                               , batteryIsRechargable :: Bool
-                               , batteryCapacity :: Double
-                               , batteryTechnology :: BatteryTechnology
-{-                               , batteryRecallNotice :: Bool
-                               , batteryRecallVendor :: Text
-                               , batteryRecallUr :: Text
+data BatteryInfo = BatteryInfo
+  { batteryNativePath :: Text
+  , batteryVendor :: Text
+  , batteryModel :: Text
+  , batterySerial :: Text
+  , batteryType :: BatteryType
+  , batteryPowerSupply :: Bool
+  , batteryHasHistory :: Bool
+  , batteryHasStatistics :: Bool
+  , batteryOnline :: Bool
+  , batteryEnergy :: Double
+  , batteryEnergyEmpty :: Double
+  , batteryEnergyFull :: Double
+  , batteryEnergyFullDesign :: Double
+  , batteryEnergyRate :: Double
+  , batteryVoltage :: Double
+  , batteryTimeToEmpty :: Int64
+  , batteryTimeToFull :: Int64
+  , batteryPercentage :: Double
+  , batteryIsPresent :: Bool
+  , batteryState :: BatteryState
+  , batteryIsRechargable :: Bool
+  , batteryCapacity :: Double
+  , batteryTechnology :: BatteryTechnology
+  }
+{-
+  , batteryUpdateTime :: Time
+  , batteryRecallNotice :: Bool
+  , batteryRecallVendor :: Text
+  , batteryRecallUr :: Text
 -}
-                               }
 
 -- | determine if a power source is a battery. The simple heuristic is a
 -- substring search on 'BAT'.
@@ -136,42 +141,47 @@ readDictIntegral dict key dflt = fromMaybe (fromIntegral dflt) $ do
 -- If some fields are not actually present, they may have bogus values
 -- here.  Don't bet anything critical on it.
 getBatteryInfo :: BatteryContext -> IO (Maybe BatteryInfo)
-getBatteryInfo (BC systemConn battPath) = do
+getBatteryInfo (BC systemConn battPath)
   -- Grab all of the properties of the battery each call with one
   -- message.
-  reply <- call_ systemConn (methodCall battPath "org.freedesktop.DBus.Properties" "GetAll")
-                             { methodCallDestination = Just "org.freedesktop.UPower"
-                             , methodCallBody = [toVariant $ T.pack "org.freedesktop.UPower.Device"]
-                             }
-
+ = do
+  reply <-
+    call_
+      systemConn
+      (methodCall battPath "org.freedesktop.DBus.Properties" "GetAll")
+      { methodCallDestination = Just "org.freedesktop.UPower"
+      , methodCallBody = [toVariant $ T.pack "org.freedesktop.UPower.Device"]
+      }
   return $ do
     body <- methodReturnBody reply `atMay` 0
     dict <- fromVariant body
-    return BatteryInfo { batteryNativePath = readDict dict "NativePath" ""
-                       , batteryVendor = readDict dict "Vendor" ""
-                       , batteryModel = readDict dict "Model" ""
-                       , batterySerial = readDict dict "Serial" ""
-                       , batteryType = toEnum $ fromIntegral $ readDictIntegral dict "Type" 0
-                       , batteryPowerSupply = readDict dict "PowerSupply" False
-                       , batteryHasHistory = readDict dict "HasHistory" False
-                       , batteryHasStatistics = readDict dict "HasStatistics" False
-                       , batteryOnline = readDict dict "Online" False
-                       , batteryEnergy = readDict dict "Energy" 0.0
-                       , batteryEnergyEmpty = readDict dict "EnergyEmpty" 0.0
-                       , batteryEnergyFull = readDict dict "EnergyFull" 0.0
-                       , batteryEnergyFullDesign = readDict dict "EnergyFullDesign" 0.0
-                       , batteryEnergyRate = readDict dict "EnergyRate" 0.0
-                       , batteryVoltage = readDict dict "Voltage" 0.0
-                       , batteryTimeToEmpty = readDict dict "TimeToEmpty" 0
-                       , batteryTimeToFull = readDict dict "TimeToFull" 0
-                       , batteryPercentage = readDict dict "Percentage" 0.0
-                       , batteryIsPresent = readDict dict "IsPresent" False
-                       , batteryState = toEnum $ readDictIntegral dict "State" 0
-                       , batteryIsRechargable = readDict dict "IsRechargable" True
-                       , batteryCapacity = readDict dict "Capacity" 0.0
-                       , batteryTechnology =
-                         toEnum $ fromIntegral $ readDictIntegral dict "Technology" 0
-                       }
+    return
+      BatteryInfo
+      { batteryNativePath = readDict dict "NativePath" ""
+      , batteryVendor = readDict dict "Vendor" ""
+      , batteryModel = readDict dict "Model" ""
+      , batterySerial = readDict dict "Serial" ""
+      , batteryType = toEnum $ fromIntegral $ readDictIntegral dict "Type" 0
+      , batteryPowerSupply = readDict dict "PowerSupply" False
+      , batteryHasHistory = readDict dict "HasHistory" False
+      , batteryHasStatistics = readDict dict "HasStatistics" False
+      , batteryOnline = readDict dict "Online" False
+      , batteryEnergy = readDict dict "Energy" 0.0
+      , batteryEnergyEmpty = readDict dict "EnergyEmpty" 0.0
+      , batteryEnergyFull = readDict dict "EnergyFull" 0.0
+      , batteryEnergyFullDesign = readDict dict "EnergyFullDesign" 0.0
+      , batteryEnergyRate = readDict dict "EnergyRate" 0.0
+      , batteryVoltage = readDict dict "Voltage" 0.0
+      , batteryTimeToEmpty = readDict dict "TimeToEmpty" 0
+      , batteryTimeToFull = readDict dict "TimeToFull" 0
+      , batteryPercentage = readDict dict "Percentage" 0.0
+      , batteryIsPresent = readDict dict "IsPresent" False
+      , batteryState = toEnum $ readDictIntegral dict "State" 0
+      , batteryIsRechargable = readDict dict "IsRechargable" True
+      , batteryCapacity = readDict dict "Capacity" 0.0
+      , batteryTechnology =
+          toEnum $ fromIntegral $ readDictIntegral dict "Technology" 0
+      }
 
 
 -- | Construct a battery context for every battery in the system. This
