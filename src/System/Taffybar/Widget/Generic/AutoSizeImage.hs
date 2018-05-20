18b98874d46dcf37d331f7972501b7bc5e2a22d7
Ivan Malison <IvanMalison@gmail.com>
2018-05-19 22:13:27 -0700

    Lookup BorderInfo at autoImageStartup instead of on every allocation
    Fixes #350. See XXX comment for why this is not a great fix.

--- src/System/Taffybar/Widget/Generic/AutoSizeImage.hs
+++ src/System/Taffybar/Widget/Generic/AutoSizeImage.hs
@@ -17,19 +17,6 @@ import           Text.Printf
 imageLog :: Priority -> String -> IO ()
 imageLog = logM "System.Taffybar.Widget.Generic.AutoSizeImage"
 
--- | Make a new image and call "autoSizeImage" on it. Automatically scale the
--- pixbuf returned from the provided getter to the appropriate size. Ignores the
--- refresh argument provided by "autoSizeImage".
-autoSizeImageNew
-  :: MonadIO m
-  => (Int32 -> IO Gdk.Pixbuf) -> Gtk.Orientation -> m Gtk.Image
-autoSizeImageNew getPixBuf orientation = do
-  image <- Gtk.imageNew
-  void $ autoSizeImage image
-         (\size -> Just <$> (getPixBuf size >>= scalePixbufToSize size orientation))
-         orientation
-  return image
-
 borderFunctions :: [Gtk.StyleContext -> [Gtk.StateFlags] -> IO Gtk.Border]
 borderFunctions =
   [ Gtk.styleContextGetPadding
@@ -65,15 +52,10 @@ addBorderInfo
   (BorderInfo t2 b2 l2 r2)
     = BorderInfo (t1 + t2) (b1 + b2) (l1 + l2) (r1 + r2)
 
--- TODO: Move this to util when gtk migration is done
-getContentAllocation :: (MonadIO m, Gtk.IsWidget a) => a -> m Gdk.Rectangle
-getContentAllocation widget = liftIO $ do
-  allocation <- Gtk.widgetGetAllocation widget
-  currentWidth <- Gdk.getRectangleWidth allocation
-  currentHeight <- Gdk.getRectangleHeight allocation
-  currentX <- Gdk.getRectangleX allocation
-  currentY <- Gdk.getRectangleX allocation
-
+-- | Get the total size of the border (the sum of its assigned margin, border
+-- and padding values) that will be drawn for a widget as a "BorderInfo" record.
+getBorderInfo :: (MonadIO m, Gtk.IsWidget a) => a -> m BorderInfo
+getBorderInfo widget = liftIO $ do
   stateFlags <- Gtk.widgetGetStateFlags widget
   styleContext <- Gtk.widgetGetStyleContext widget
 
@@ -82,7 +64,21 @@ getContentAllocation widget = liftIO $ do
       combineBorderInfo lastSum fn =
         addBorderInfo lastSum <$> getBorderInfoFor fn
 
-  borderInfo <- foldM combineBorderInfo borderInfoZero borderFunctions
+  foldM combineBorderInfo borderInfoZero borderFunctions
+
+-- | Get the actual allocation for a "Gtk.Widget", accounting for the size of
+-- its CSS assined margin, border and padding values.
+getContentAllocation
+  :: (MonadIO m, Gtk.IsWidget a)
+  => a -> BorderInfo -> m Gdk.Rectangle
+getContentAllocation widget borderInfo = do
+  borderInfo <- getBorderInfo widget
+
+  allocation <- Gtk.widgetGetAllocation widget
+  currentWidth <- Gdk.getRectangleWidth allocation
+  currentHeight <- Gdk.getRectangleHeight allocation
+  currentX <- Gdk.getRectangleX allocation
+  currentY <- Gdk.getRectangleX allocation
 
   Gdk.setRectangleWidth allocation $ max 1 $
      currentWidth - fromIntegral (borderWidth borderInfo)
@@ -92,48 +88,69 @@ getContentAllocation widget = liftIO $ do
      currentX + fromIntegral (borderLeft borderInfo)
   Gdk.setRectangleY allocation $
      currentY + fromIntegral (borderTop borderInfo)
+
   return allocation
 
--- | Automatically update an images pixbuf using the provided action whenever
--- the image is allocated. Returns an action that forces a refresh of the image
--- through the provided action.
+-- | Automatically update the "Gdk.Pixbuf" of a "Gtk.Image" using the provided
+-- action whenever the "Gtk.Image" is allocated. Returns an action that forces a
+-- refresh of the image through the provided action.
 autoSizeImage
   :: MonadIO m
   => Gtk.Image
   -> (Int32 -> IO (Maybe Gdk.Pixbuf))
   -> Gtk.Orientation
   -> m (IO ())
-autoSizeImage image getPixBuf orientation = liftIO $ do
-  Gtk.widgetSetVexpand image True
+autoSizeImage image getPixbuf orientation = liftIO $ do
+  case orientation of
+    Gtk.OrientationHorizontal -> Gtk.widgetSetVexpand image True
+    _ -> Gtk.widgetSetHexpand image True
   widgetSetClassGI image "AutoSizeImage"
+
   lastAllocation <- MV.newMVar 0
-  let setPixbuf force actualAllocation = do
-        allocation <- getContentAllocation image
-        currentWidth <- Gdk.getRectangleWidth allocation
-        currentHeight <- Gdk.getRectangleHeight allocation
-        actualWidth <- Gdk.getRectangleWidth actualAllocation
-        actualHeight <- Gdk.getRectangleHeight actualAllocation
+  -- XXX: Gtk seems to report information about padding etc inconsistently,
+  -- which is why we look it up once, at startup. This means that we won't
+  -- properly react to changes to these values, which could be a pretty nasty
+  -- gotcha for someone down the line. :(
+  borderInfo <- getBorderInfo image
 
-        let size =
+  let setPixbuf force allocation = do
+        _width <- Gdk.getRectangleWidth allocation
+        _height <- Gdk.getRectangleHeight allocation
+
+        let width = max 1 $ _width - fromIntegral (borderWidth borderInfo)
+            height = max 1 $ _height - fromIntegral (borderHeight borderInfo)
+            size =
               case orientation of
-                Gtk.OrientationHorizontal -> currentHeight
-                _ -> currentWidth
+                Gtk.OrientationHorizontal -> height
+                _ -> width
 
-        requestResize <- MV.modifyMVar lastAllocation $ \previous ->
-          return (size, size /= previous)
-        imageLog DEBUG $
+        previousSize <- MV.readMVar lastAllocation
+
+        when (size /= previousSize || force) $ do
+          MV.modifyMVar_ lastAllocation $ const $ return size
+          imageLog DEBUG $
                  printf "Allocating image: size %s, width %s, \
-                         \ height %s, do resize %s, aw: %s, ah: %s"
+                         \ height %s, aw: %s, ah: %s"
                  (show size)
-                 (show currentWidth)
-                 (show currentHeight)
-                 (show requestResize)
-                 (show actualWidth)
-                 (show actualHeight)
-        when (requestResize || force) $ do
-          imageLog DEBUG "Requesting resize"
-          getPixBuf size >>= Gtk.imageSetFromPixbuf image
+                 (show width)
+                 (show height)
+                 (show _width)
+                 (show _height)
+          getPixbuf size >>= Gtk.imageSetFromPixbuf image
           Gtk.postGUIASync $ Gtk.widgetQueueResize image
 
   _ <- Gtk.onWidgetSizeAllocate image $ setPixbuf False
   return $ Gtk.widgetGetAllocation image >>= setPixbuf True
+
+-- | Make a new "Gtk.Image" and call "autoSizeImage" on it. Automatically scale
+-- the "Gdk.Pixbuf" returned from the provided getter to the appropriate size
+-- using "scalePixbufToSize".
+autoSizeImageNew
+  :: MonadIO m
+  => (Int32 -> IO Gdk.Pixbuf) -> Gtk.Orientation -> m Gtk.Image
+autoSizeImageNew getPixBuf orientation = do
+  image <- Gtk.imageNew
+  void $ autoSizeImage image
+         (\size -> Just <$> (getPixBuf size >>= scalePixbufToSize size orientation))
+         orientation
+  return image
