fd6782eb76e247770e05d19c2230ccf228ac0aa1
Kirill Zaborsky <qrilka@gmail.com>
2019-02-09 18:38:40 +0300

    Fix station search URL
    Previous URL is no longer accessible and actually www.weather.gov contains forms forwarding requests to not available DNS name weather.noaa.gov

--- src/System/Taffybar/Widget/Weather.hs
+++ src/System/Taffybar/Widget/Weather.hs
@@ -2,7 +2,7 @@
 -- | This module defines a simple textual weather widget that polls
 -- NOAA for weather data.  To find your weather station, you can use
 --
--- <http://www.nws.noaa.gov/tg/siteloc.php>
+-- <https://www.weather.gov/tg/siteloc>
 --
 -- For example, Madison, WI is KMSN.
 --
