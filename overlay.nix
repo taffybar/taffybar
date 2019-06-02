fdf4f7404e22091bcc0899188c01964b87c68f47
Ivan Malison <IvanMalison@gmail.com>
2019-06-02 11:40:26 -0700

    Separate overlay from default.nix

---
+++ overlay.nix
@@ -0,0 +1,18 @@
+pkgs: rec {
+  haskellPackages = pkgs.haskellPackages.override {
+    overrides = self: super: rec {
+      taffybar = pkgs.haskell.lib.overrideCabal (super.taffybar.overrideAttrs (oldAttrs: rec {
+        src = ./.;
+      })) (oldDerivation: {
+        libraryHaskellDepends = oldDerivation.libraryHaskellDepends ++ [ self.broadcast-chan ];
+      });
+      broadcast-chan = pkgs.haskell.lib.overrideCabal super.broadcast-chan (_: {
+        version = "0.2.0.2";
+        sha256 = "12ax37y9i3cs8wifz01lpq0awm9c235l5xkybf13ywvyk5svb0jv";
+        revision = null;
+        editedCabalFile = null;
+        broken = false;
+      });
+    };
+  };
+}
