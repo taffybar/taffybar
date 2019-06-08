084f314ee8807c517b8a65a95b39a55ed17879f9
Ivan Malison <IvanMalison@gmail.com>
2019-06-08 12:36:59 -0700

    Only rexport modules in System.Taffybar.Widget

--- src/System/Taffybar/Widget.hs
+++ src/System/Taffybar/Widget.hs
@@ -4,33 +4,28 @@ module System.Taffybar.Widget
   , module System.Taffybar.Widget.Battery
 
   -- * "System.Taffybar.Widget.CPUMonitor"
-  , cpuMonitorNew
+  , module System.Taffybar.Widget.CPUMonitor
 
   -- * "System.Taffybar.Widget.CommandRunner"
-  , commandRunnerNew
+  , module System.Taffybar.Widget.CommandRunner
 
   -- * "System.Taffybar.Widget.Decorators"
   , module System.Taffybar.Widget.Decorators
 
   -- * "System.Taffybar.Widget.DiskIOMonitor"
-  , dioMonitorNew
+  , module System.Taffybar.Widget.DiskIOMonitor
 
   -- * "System.Taffybar.Widget.FSMonitor"
-  , fsMonitorNew
+  , module System.Taffybar.Widget.FSMonitor
 
   -- * "System.Taffybar.Widget.FreedesktopNotifications"
-  , Notification(..)
-  , NotificationConfig(..)
-  , defaultNotificationConfig
-  , notifyAreaNew
+  , module System.Taffybar.Widget.FreedesktopNotifications
 
   -- * "System.Taffybar.Widget.Layout"
-  , LayoutConfig(..)
-  , defaultLayoutConfig
-  , layoutNew
+  , module System.Taffybar.Widget.Layout
 
   -- * "System.Taffybar.Widget.MPRIS2"
-  , mpris2New
+  , module System.Taffybar.Widget.MPRIS2
 
   -- * "System.Taffybar.Widget.NetworkGraph"
   , module System.Taffybar.Widget.NetworkGraph
@@ -42,7 +37,7 @@ module System.Taffybar.Widget
   , module System.Taffybar.Widget.SimpleClock
 
   -- * "System.Taffybar.Widget.SimpleCommandButton"
-  , simpleCommandButtonNew
+  , module System.Taffybar.Widget.SimpleCommandButton
 
   -- * "System.Taffybar.Widget.Text.CPUMonitor"
   , module System.Taffybar.Widget.Text.CPUMonitor
@@ -54,19 +49,10 @@ module System.Taffybar.Widget
   , module System.Taffybar.Widget.Text.NetworkMonitor
 
   -- * "System.Taffybar.Widget.Weather"
-  , WeatherConfig(..)
-  , WeatherInfo(..)
-  , WeatherFormatter(WeatherFormatter)
-  , weatherNew
-  , weatherCustomNew
-  , defaultWeatherConfig
+  , module System.Taffybar.Widget.Weather
 
   -- * "System.Taffybar.Widget.Windows"
-  , windowsNew
-  , WindowsConfig(..)
-  , defaultWindowsConfig
-  , truncatedGetActiveLabel
-  , truncatedGetMenuLabel
+  , module System.Taffybar.Widget.Windows
 
   -- * "System.Taffybar.Widget.Workspaces"
   , module System.Taffybar.Widget.Workspaces
