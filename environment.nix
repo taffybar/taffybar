7d2c5f1e711a07dc49b688fbb7f16129247111b7
Ivan Malison <IvanMalison@gmail.com>
2019-06-18 12:28:35 -0700

    Rename overlay to environment, removing taffybar source

--- environment.nix
+++ environment.nix
@@ -1,6 +1,5 @@
 _: pkgs:
 let
-  sourceTransformer = if builtins.getEnv "CI" == "" then builtins.fetchGit else (x: x);
   addGObjectIntrospection = hpackage: pkgs.haskell.lib.overrideCabal hpackage (current: {
     libraryPkgconfigDepends =
       current.libraryPkgconfigDepends ++ [ pkgs.gobject-introspection ];
@@ -8,7 +7,6 @@ let
 in {
   haskellPackages = pkgs.haskellPackages.override (old: {
     overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: {})) (self: super: {
-      taffybar = self.callCabal2nix "taffybar" (sourceTransformer ./.) { inherit (pkgs) gtk3; };
       broadcast-chan = pkgs.haskell.lib.overrideCabal super.broadcast-chan (_: {
         version = "0.2.0.2";
         sha256 = "12ax37y9i3cs8wifz01lpq0awm9c235l5xkybf13ywvyk5svb0jv";
