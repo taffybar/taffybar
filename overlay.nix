b3ca25b0f6656f2da97c8701247519f9300a28c3
Ivan Malison <IvanMalison@gmail.com>
2019-06-18 12:28:52 -0700

    Add overlay.nix which combines taffybar override with environment override

---
+++ overlay.nix
@@ -0,0 +1,12 @@
+self: super:
+
+let
+  sourceTransformer = if builtins.getEnv "CI" == "" then builtins.fetchGit else (x: x);
+  taffybarOverlay = _: pkgs: {
+    haskellPackages = pkgs.haskellPackages.override (old: {
+      overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: {})) (self: super: {
+        taffybar = self.callCabal2nix "taffybar" (sourceTransformer ./.) { inherit (pkgs) gtk3; };
+      });
+    });
+  };
+in super.lib.composeExtensions taffybarOverlay (import ./environment.nix) self super
