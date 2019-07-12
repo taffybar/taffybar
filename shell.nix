dd1cb51c76e15a4384a4317ce46fddcd35785b56
Ivan Malison <IvanMalison@gmail.com>
2019-07-12 11:31:52 -0700

    Fix whitespace in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 let
   overlays = [ (import ./overlay.nix) ];
   pkgs = (import ./nixpkgs.nix) { inherit overlays; };
-in pkgs.haskellPackages.shellFor { packages = p: [p.taffybar]; }
+in pkgs.haskellPackages.shellFor { packages = p: [ p.taffybar ]; }
