9a7adb5fb1a0728b2cbae23ca6011d70eace92ac
Ivan Malison <IvanMalison@gmail.com>
2020-02-29 18:20:53 -0800

    Revert "Patch dbus as a temporary solution to #477"
    This reverts commit d19f8dd4c9d19471aa19c42e6df729867f39379e.

--- environment.nix
+++ environment.nix
@@ -111,7 +111,6 @@ in {
         sha256 = "0pizgz6846i19fi0avsqlqxal1f09ihpd9qlyyhci4j3fvgvrsl0";
       }
       { inherit (pkgs) glib gobject-introspection; };
-      dbus = pkgs.haskell.lib.appendPatch super.dbus ./dbus.patch;
     });
   });
 }
