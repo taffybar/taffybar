7b2e38b1a7b96b0ef71e4b1f2a8c2d15ee3227c8
Rodney Lorrimar <dev@rodney.id.au>
2021-05-12 20:35:05 +1000

    nix: add gtk-sni-tray build fix

--- overlay.nix
+++ overlay.nix
@@ -2,7 +2,7 @@ self: super:
 
 let
   sourceTransformer = if builtins.getEnv "CI" == "" then builtins.fetchGit else (x: x);
-  taffybarOverlay = _: pkgs: {
+  taffybarOverlay = _: pkgs: with pkgs.haskell.lib; {
     haskellPackages = pkgs.haskellPackages.override (old: {
       overrides =
         pkgs.lib.composeExtensions (old.overrides or (_: _: {}))
@@ -10,6 +10,11 @@ let
           taffybar =
             self.callCabal2nix "taffybar" (sourceTransformer ./.)
             { inherit (pkgs) gtk3; };
+          # https://github.com/taffybar/gtk-sni-tray/pull/25
+          gtk-sni-tray = markUnbroken (appendPatch super.gtk-sni-tray (pkgs.fetchpatch {
+            url = "https://github.com/rvl/gtk-sni-tray/commit/4afd84654cb3f2bd2bb7d39451706c5914fd3cdf.patch";
+            sha256 = "1xjxlh58vnykqsjq4qw8mliq3gk17mwxi4h9z8dvjyav8zqg05rn";
+          }));
         });
     });
   };
