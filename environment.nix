e615fcabe45e645421e8e837d42ea81253428c78
Ivan Malison <IvanMalison@gmail.com>
2021-05-16 00:26:30 -0600

    Add gtk-sni-tray 0.1.6.2 to environment overlay

--- environment.nix
+++ environment.nix
@@ -7,6 +7,11 @@ let
 in {
   haskellPackages = pkgs.haskellPackages.override (old: {
     overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: {})) (self: super: {
+      gtk-sni-tray = self.callHackageDirect {
+        pkg = "gtk-sni-tray";
+        ver = "0.1.6.2";
+        sha256 = "13m6klwx0nc58h7fjss2jwcmk2y8kakcrrc741vbfnnap83achv5";
+      } {inherit (pkgs) gtk3;};
     });
   });
 }
