6e68e5da86bc85ebbec6f71fdb631113aeeae85d
Ivan Malison <IvanMalison@gmail.com>
2021-08-06 23:31:06 -0600

    Multi-line

--- src/System/Taffybar/Widget/Generic/ChannelWidget.hs
+++ src/System/Taffybar/Widget/Generic/ChannelWidget.hs
@@ -7,7 +7,9 @@ import Control.Monad.IO.Class
 import Data.Foldable (traverse_)
 import GI.Gtk
 
-channelWidgetNew :: (MonadIO m, IsWidget w) => w -> BroadcastChan In a -> (a -> IO ()) -> m w
+channelWidgetNew ::
+  (MonadIO m, IsWidget w) =>
+  w -> BroadcastChan In a -> (a -> IO ()) -> m w
 channelWidgetNew widget channel updateWidget = do
   void $ onWidgetRealize widget $ do
     ourChan <- newBChanListener channel
