27b315dc8f888afa6c83610dc656dc9d41564d6b
Ivan Malison <IvanMalison@gmail.com>
2021-08-08 01:52:29 -0600

    Fix printfs

--- src/System/Taffybar/Context.hs
+++ src/System/Taffybar/Context.hs
@@ -205,14 +205,14 @@ buildBarWindow context barConfig = do
   let addWidgetWith widgetAdd (count, buildWidget) =
         runReaderT buildWidget thisContext >>= widgetAdd count
       addToStart count widget = do
-        _ <- widgetSetClassGI box $ T.pack $ printf "left-%s" (count :: Int)
+        _ <- widgetSetClassGI box $ T.pack $ printf "left-%d" (count :: Int)
         Gtk.boxPackStart box widget False False 0
       addToEnd count widget = do
-        _ <- widgetSetClassGI box $ T.pack $ printf "right-%s" (count :: Int)
+        _ <- widgetSetClassGI box $ T.pack $ printf "right-%d" (count :: Int)
         Gtk.boxPackEnd box widget False False 0
       addToCenter count widget = do
         alignCenter widget
-        _ <- widgetSetClassGI box $ T.pack $ printf "center-%s" (count :: Int)
+        _ <- widgetSetClassGI box $ T.pack $ printf "center-%d" (count :: Int)
         Gtk.boxPackStart centerBox widget False False 0
 
   logIO DEBUG "Building start widgets"
