a45f84b4e870258554d10194e0446e2dd310df9e
Ivan Malison <IvanMalison@gmail.com>
2023-05-16 15:57:49 -0600

    Refactor flake to allow specification of custom compiler

--- flake.nix
+++ flake.nix
@@ -4,47 +4,53 @@
     git-ignore-nix.url = github:hercules-ci/gitignore.nix/master;
     gtk-sni-tray.url = github:taffybar/gtk-sni-tray/master;
     gtk-strut.url = github:taffybar/gtk-strut/master;
+    xmonad.url = github:xmonad/xmonad/master;
   };
   outputs = {
-    self, flake-utils, nixpkgs, git-ignore-nix, gtk-sni-tray, gtk-strut
+    self, flake-utils, nixpkgs, git-ignore-nix, gtk-sni-tray, gtk-strut, xmonad
   }:
   let
-    overlay = final: prev: {
-      haskellPackages = prev.haskellPackages.override (old: {
-        overrides = prev.lib.composeExtensions (old.overrides or (_: _: {}))
-        (hself: hsuper: {
+    defComp = if builtins.pathExists ./comp.nix
+      then import ./comp.nix
+      else { };
+    hpath = { prefix ? null, compiler ? null }:
+      (if prefix == null then [] else [ prefix ]) ++
+      (if compiler == null
+       then [ "haskellPackages" ]
+       else [ "haskell" "packages" compiler ]
+      );
+    hoverlay = final: prev: hself: hsuper: {
+      taffybar =
+        hself.callCabal2nix "taffybar"
+        (git-ignore-nix.lib.gitignoreSource ./.)
+        { inherit (final) gtk3;  };
 
-          taffybar =
-            hself.callCabal2nix "taffybar"
-            (git-ignore-nix.lib.gitignoreSource ./.)
-            { inherit (final) gtk3;  };
-
-          dyre = prev.haskell.lib.dontCheck (hself.callHackageDirect {
-            pkg = "dyre";
-            ver = "0.9.1";
-            sha256 = "sha256-3ClPPbNm5wQI+QHaR0Rtiye2taSTF3IlWgfanud6wLg=";
-          } { });
-
-        });
-      });
+        dyre = prev.haskell.lib.dontCheck (hself.callHackageDirect {
+          pkg = "dyre";
+          ver = "0.9.1";
+          sha256 = "sha256-3ClPPbNm5wQI+QHaR0Rtiye2taSTF3IlWgfanud6wLg=";
+        } { });
     };
+    overlay = xmonad.lib.fromHOL hoverlay defComp;
     overlays = gtk-strut.overlays ++ gtk-sni-tray.overlays ++ [ overlay ];
   in flake-utils.lib.eachDefaultSystem (system:
   let pkgs = import nixpkgs { inherit system overlays; config.allowBroken = true; };
+      hpkg = pkgs.lib.attrsets.getAttrFromPath (hpath defComp) pkgs;
   in
   rec {
-    devShell = pkgs.haskellPackages.shellFor {
+    devShell = hpkg.shellFor {
       packages = p: [ p.taffybar ];
-      nativeBuildInputs = with pkgs.haskellPackages; [
+      nativeBuildInputs = with hpkg; [
         cabal-install hlint ghcid ormolu implicit-hie haskell-language-server
       ];
 
-      buildInputs = with pkgs; [ pcre.dev util-linux.dev libselinux.dev
+      buildInputs = with pkgs; [
+        pcre.dev pcre2.dev util-linux.dev libselinux.dev
         libsepol.dev libthai.dev libdatrie.dev xorg.libXdmcp.dev
-        libxkbcommon.dev libepoxy.dev dbus.dev at-spi2-core.dev xorg.libXtst ];
-
+        libxkbcommon.dev libepoxy.dev dbus.dev at-spi2-core.dev xorg.libXtst
+      ];
     };
-    buildInputs = [ pkgs.haskellPackages.cabal-install ];
-    defaultPackage = pkgs.haskellPackages.taffybar;
+    buildInputs = [ hpkg.cabal-install ];
+    defaultPackage = hpkg.taffybar;
   }) // { inherit overlay overlays; } ;
 }
