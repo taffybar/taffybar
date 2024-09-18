7579d22ec8f16bdc9cf74391880dc3f3dbd95a45
Rodney Lorrimar <dev@rodney.id.au>
2024-09-18 21:32:59 +0800

    Auth.passGet: Remove use of partial functions & incomplete patterns

--- src/System/Taffybar/Auth.hs
+++ src/System/Taffybar/Auth.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module System.Taffybar.Auth where
 
-import           Control.Arrow
 import           Control.Monad.IO.Class
 import           Data.Maybe
 import           System.Taffybar.Util
@@ -11,13 +10,13 @@ fieldRegex :: Regex
 fieldRegex = mkRegexWithOpts "^(.*?): *(.*?)$" True True
 
 passGet :: MonadIO m => String -> m (Either String (String, [(String, String)]))
-passGet credentialName =
-  right (getPassComponents . lines) <$>
-        runCommandFromPath ["pass", "show", credentialName]
-  where getPassComponents passLines =
-          let entries =
-                map buildEntry $ catMaybes $
-                    matchRegex fieldRegex <$> tail passLines
-              buildEntry [fieldName, fieldValue] = (fieldName, fieldValue)
-              buildEntry _ = ("", "")
-          in (head passLines, entries)
+passGet credentialName = (>>= getPassComponents . lines) <$> runPassShow
+  where runPassShow = runCommandFromPath ["pass", "show", credentialName]
+
+        getPassComponents [] = Left "pass show command produced no output"
+        getPassComponents (key:rest) = Right (key, buildEntries rest)
+
+        buildEntries = mapMaybe buildEntry . mapMaybe (matchRegex fieldRegex)
+
+        buildEntry [fieldName, fieldValue] = Just (fieldName, fieldValue)
+        buildEntry _ = Nothing
