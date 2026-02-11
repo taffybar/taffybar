c3434887f5d6870cae1e3b555e2c4a2b7f08f1e8
Ivan Malison <IvanMalison@gmail.com>
2026-02-11 00:29:43 -0800

    Refactor workspace modules under Workspaces and add EWMH canonical API

--- src/System/Taffybar/Widget/Workspaces/Common.hs
+++ src/System/Taffybar/Widget/Workspaces/Common.hs
@@ -3,16 +3,17 @@
 -----------------------------------------------------------------------------
 
 -- |
--- Module      : System.Taffybar.Widget.HyprlandWorkspaces
+-- Module      : System.Taffybar.Widget.Workspaces.Common
 -- Copyright   : (c) Ivan A. Malison
 -- License     : BSD3-style (see LICENSE)
 --
 -- Maintainer  : Ivan A. Malison
 -- Stability   : unstable
 -- Portability : unportable
-module System.Taffybar.Widget.HyprlandWorkspaces
-  ( module System.Taffybar.Widget.Workspaces.Hyprland,
+module System.Taffybar.Widget.Workspaces.Common
+  {-# DEPRECATED "Use System.Taffybar.Widget.Workspaces.Config" #-}
+  ( module System.Taffybar.Widget.Workspaces.Config,
   )
 where
 
-import System.Taffybar.Widget.Workspaces.Hyprland
+import System.Taffybar.Widget.Workspaces.Config
