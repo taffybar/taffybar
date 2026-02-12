8e3f9c75df66758aba3b64425706d2261a15be5e
Ivan Malison <IvanMalison@gmail.com>
2026-02-11 18:27:34 -0800

    Pin status-notifier-item fix for CI D-Bus tests

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
       flake = false;
     };
     status-notifier-item = {
-      url = "github:taffybar/status-notifier-item";
+      url = "github:taffybar/status-notifier-item?ref=fix/dbus-session-config-tests";
       flake = false;
     };
     weeder-nix = {
